902785d694358f0fce60b5b7ae789ccb2b1c0cb4
Wojciech Nowak <mail@pythonic.ninja>
2017-01-10 14:35:28 +0100

    Escape real dots in regex used for oEmbed

--- includes/class-wordpress-villoid-store-oembed.php
+++ includes/class-wordpress-villoid-store-oembed.php
@@ -7,8 +7,8 @@ class oEmbedVilloid {
 
   function __construct()
   {
-    $this->oembed_endpoint = 'https://.*.villoid.com/oembed/';
-    $this->oembed_format = '#https?://(www\\.)?.*villoid.com/.*#i';
+    $this->oembed_endpoint = 'https://.*\.villoid.com/oembed/';
+    $this->oembed_format = '#https?://(www\\.)?.*\.villoid.com/.*#i';
 
     $this->new_oembed();
   }
